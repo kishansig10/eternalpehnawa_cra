5d1f846f9b4f92c38120fe9523b351a8328f8d86
isthatsahil <sahil.verma997@gmail.com>
2022-02-27 23:10:37 +0530

    Init CRA

--- src/components/checkout/FormPayment.tsx
+++ src/components/checkout/FormPayment.tsx
@@ -57,7 +57,7 @@ const FormPayment = ({ backStep, customerShippingData, nextStep }) => {
 
     try {
       const data = await axios.post(
-        "https://youthful-kirch-24c6e7.netlify.app/.netlify/functions/razorpay",
+        "/.netlify/functions/razorpay",
         {
           orderAmount: checkoutToken?.live?.total_with_tax?.raw,
         },
