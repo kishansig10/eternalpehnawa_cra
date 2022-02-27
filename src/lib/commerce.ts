0f9eb1767773e41f52014b83219b43d2138ec532
isthatsahil <sahil.verma997@gmail.com>
2022-02-27 23:16:39 +0530

    Env keys

--- src/lib/commerce.ts
+++ src/lib/commerce.ts
@@ -1,14 +1,3 @@
 import Commerce from "@chec/commerce.js";
 
-export const commerce = new Commerce(
-  "pk_36070c809d0424b5962d42ef106bee39723ae3a6e803c",
-  "development"
-);
-
-export const login = () => {
-  commerce.customer
-    .login("sahil.verma997@gmail.com", "http://localhost:3001/")
-    .then((token) => {
-      console.log("token", token);
-    });
-};
+export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY);
